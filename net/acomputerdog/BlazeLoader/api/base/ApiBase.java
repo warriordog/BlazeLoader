a84d65f5f60ad7e9f2b181ce56bbf479fe1cd619
warriordog <acomputerdog@gmail.com>
2013-10-07 18:33:34 -0400

    Add ApiBase.shutdown() and ApiBase.shutdown(int) to allow mods to shut down the game; update javadocs

--- net/acomputerdog/BlazeLoader/api/base/ApiBase.java
+++ net/acomputerdog/BlazeLoader/api/base/ApiBase.java
@@ -1,5 +1,6 @@
 package net.acomputerdog.BlazeLoader.api.base;
 
+import net.acomputerdog.BlazeLoader.main.BlazeLoader;
 import net.minecraft.src.EntityClientPlayerMP;
 import net.minecraft.src.ILogAgent;
 import net.minecraft.src.Minecraft;
@@ -35,4 +36,19 @@ public class ApiBase {
      * Location of the working directory for mods.  Mods should load and save configurations, resources, etc. here.
      */
     public static File modDir = new File("./mods/");
+
+    /**
+     * Shuts down the game with a specified error code.  Use 0 for normal shutdown.
+     * @param code The error code to return to the system after shutdown.
+     */
+    public static void shutdown(int code){
+        BlazeLoader.shutdown(code);
+    }
+
+    /**
+     * Shuts down the game with error code 0 (no error).
+     */
+    public static void shutdown(){
+        shutdown(0);
+    }
 }
