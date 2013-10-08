2ff86b5851cc243cd98eb802260c0ba6d41c406b
warriordog <acomputerdog@gmail.com>
2013-10-08 12:14:13 -0400

    Add ApiGui

---
+++ net/acomputerdog/BlazeLoader/api/gui/ApiGUI.java
@@ -0,0 +1,33 @@
+package net.acomputerdog.BlazeLoader.api.gui;
+
+import net.acomputerdog.BlazeLoader.api.base.ApiBase;
+import net.minecraft.src.GuiScreen;
+
+/**
+ * API functions for GUI-related tasks.
+ */
+public class ApiGui {
+
+    /**
+     * Opens a GUI, triggering an eventDisplayGui in the process.
+     * @param gui The GUI to display.
+     */
+    public static void openGUI(GuiScreen gui){
+        ApiBase.theMinecraft.displayGuiScreen(gui);
+    }
+
+    /**
+     * Closes the currently open GUI, returning to the bottom layer.  (Usually either the main menu or the main game interface.)
+     */
+    public static void closeCurrentGUI(){
+        ApiBase.theMinecraft.displayGuiScreen(null);
+    }
+
+    /**
+     * Returns the currently open GUI.  May be null.
+     * @return Returns the currently open GUI.
+     */
+    public static GuiScreen getOpenGUI(){
+        return ApiBase.theMinecraft.currentScreen;
+    }
+}
