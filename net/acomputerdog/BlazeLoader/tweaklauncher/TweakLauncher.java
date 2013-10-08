71a117ff683f5f757d494d386ac0cfb5bf6dcaf2
warriordog <acomputerdog@gmail.com>
2013-10-08 10:14:26 -0400

    Start work on TweakLauncher

---
+++ net/acomputerdog/BlazeLoader/tweaklauncher/TweakLauncher.java
@@ -0,0 +1,27 @@
+package net.acomputerdog.BlazeLoader.tweaklauncher;
+
+import net.minecraft.launchwrapper.ITweaker;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+
+import java.io.File;
+import java.util.List;
+
+public class TweakLauncher implements ITweaker {
+    @Override
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
+    }
+
+    @Override
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+    }
+
+    @Override
+    public String getLaunchTarget() {
+        return null;
+    }
+
+    @Override
+    public String[] getLaunchArguments() {
+        return new String[0];
+    }
+}
