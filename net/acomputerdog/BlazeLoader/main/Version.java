af98ff9b99f8f25dc429d2e8064ffa301fa37d2f
warriordog <acomputerdog@gmail.com>
2013-10-14 14:32:13 -0400

    Update to Version.class.  Separate versions into global, API, and internal changes.

--- net/acomputerdog/BlazeLoader/main/Version.java
+++ net/acomputerdog/BlazeLoader/main/Version.java
@@ -7,20 +7,49 @@ package net.acomputerdog.BlazeLoader.main;
 public class Version {
 
     /**
-     * Gets the version of BlazeLoader as an integer.  Incremented by one with every commit.
-     * Should to be used to compare versions.
-     * @return Returns the version of BlazeLoader as an integer.
+     * Gets the global version of BlazeLoader.  A change here will RESET the status of the other two update counters.
+     * Mods will be unable to check the status of the other two counters so changes here must be taken into account.
+     * Generally a change here is either a Minecraft update or a large restructuring.
+     * @return Return the global version of BlazeLoader.
      */
-    public static int getIntVersion(){
-        return 6;
+    public static int getGlobalVersion(){
+        return 0;
     }
 
     /**
-     * Gets the version of BlazeLoader as a string formatted for display.  Example return: "dev_0.1.234"
+     * Gets the version of the API features of BlazeLoader.  Incremented with changes to package "api", package "util", and Class Mod.
+     * Changes here may affect mods.
+     * @return Get the version of the API features of BlazeLoader
+     */
+    public static int getApiVersion(){
+        return 0;
+    }
+
+    /**
+     * Gets the version of the internal features of BlazeLoader.
+     * Mods that only use package "api", package "util", and Class Mod should be unaffected by version changes here.
+     * @return Return an int representing the version of BL's internal components.
+     */
+    public static int getInternalVersion(){
+        return 0;
+    }
+
+    /**
+     * Gets the version of BlazeLoader as an integer.  Incremented by one with every commit.
+     * Should to be used to compare versions.
+     * -Will be removed soon!-
+     * @return Returns the version of BlazeLoader as an integer.
+     */
+    public static int getIntVersion(){
+        return 7;
+    }
+
+    /**
+     * Gets the version of BlazeLoader as a string formatted for display.  Example return: "0.1.234"
      * @return Returns the version of BlazeLoader as a String formatted for display.
      */
     public static String getStringVersion(){
-        return "dev_0.0.006";
+        return getGlobalVersion() + "." + getApiVersion() + "." + getInternalVersion();
     }
 
     /**
