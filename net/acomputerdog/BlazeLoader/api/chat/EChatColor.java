299d737a49b5c627e814319d0c20077ba4f48855
warriordog <acomputerdog@gmail.com>
2013-10-23 16:37:21 -0400

    Hopefully fixed bug with incorrect chat formatting in compiled versions of the game.

--- net/acomputerdog/BlazeLoader/api/chat/EChatColor.java
+++ net/acomputerdog/BlazeLoader/api/chat/EChatColor.java
@@ -4,30 +4,31 @@ package net.acomputerdog.BlazeLoader.api.chat;
  * Chat formatting markers.
  */
 public enum EChatColor {
-    COLOR_AQUA("§b"),
-    COLOR_BLACK("§0"),
-    COLOR_BLUE("§9"),
-    COLOR_ORANGE("§6"),
-    COLOR_GRAY("§7"),
-    COLOR_GREEN("§a"),
-    COLOR_PURPLE("§d"),
-    COLOR_RED("§c"),
-    COLOR_WHITE("§f"),
-    COLOR_YELLOW("§e"),
-    COLOR_DARK_AQUA("§3"),
-    COLOR_DARK_BLUE("§1"),
-    COLOR_DARK_GRAY("§8"),
-    COLOR_DARK_GREEN("§2"),
-    COLOR_DARK_RED("§4"),
-    COLOR_DARK_PURPLE("§5"),
-    FORMAT_BOLD("§l"),
-    FORMAT_ITALIC("§o"),
-    FORMAT_RANDOM("§k"),
-    FORMAT_RESET("§p"),
-    FORMAT_STRIKE("§m"),
-    FORMAT_UNDERLINE("§n");
+    COLOR_AQUA("b"),
+    COLOR_BLACK("0"),
+    COLOR_BLUE("9"),
+    COLOR_ORANGE("6"),
+    COLOR_GRAY("7"),
+    COLOR_GREEN("a"),
+    COLOR_PURPLE("d"),
+    COLOR_RED("c"),
+    COLOR_WHITE("f"),
+    COLOR_YELLOW("e"),
+    COLOR_DARK_AQUA("3"),
+    COLOR_DARK_BLUE("1"),
+    COLOR_DARK_GRAY("8"),
+    COLOR_DARK_GREEN("2"),
+    COLOR_DARK_RED("4"),
+    COLOR_DARK_PURPLE("5"),
+    FORMAT_BOLD("l"),
+    FORMAT_ITALIC("o"),
+    FORMAT_RANDOM("k"),
+    FORMAT_RESET("p"),
+    FORMAT_STRIKE("m"),
+    FORMAT_UNDERLINE("n");
 
     protected String code;
+    private static final String sectionSign = String.valueOf('\u00a7');
 
     EChatColor(String code){
         this.code = code;
@@ -38,7 +39,7 @@ public enum EChatColor {
      * @return Return the color code associated with this EChatColor.
      */
     public String get(){
-        return code;
+        return sectionSign + code;
     }
 
     /**
--- net/acomputerdog/BlazeLoader/main/Version.java
+++ net/acomputerdog/BlazeLoader/main/Version.java
@@ -31,7 +31,7 @@ public final class Version {
      * @return Return an int representing the version of BL's internal components.
      */
     public static int getInternalVersion(){
-        return 8;
+        return 9;
     }
 
     @Deprecated
@@ -41,7 +41,7 @@ public final class Version {
      * @return Returns the version of BlazeLoader as an integer.
      */
     public static int getIntVersion(){
-        return 20;
+        return 21;
     }
 
     /**
