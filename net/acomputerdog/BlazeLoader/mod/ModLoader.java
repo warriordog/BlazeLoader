7f4513bc97421a419210d9fb162fa2bf656fc98a
warriordog <acomputerdog@gmail.com>
2013-11-01 07:32:42 -0400

    Refactor ModLoader

--- net/acomputerdog/BlazeLoader/mod/ModLoader.java
+++ net/acomputerdog/BlazeLoader/mod/ModLoader.java
@@ -8,6 +8,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -17,6 +18,9 @@ import java.util.zip.ZipFile;
  * Handles detecting and loading mods.
  */
 public class ModLoader {
+    public static void loadModsToList(File searchDir){
+        loadMods(searchDir, ModList.getUnloadedMods());
+    }
 
     public static void loadMods(File searchDir, List<Class> modList){
         ApiBase.theProfiler.startSection("load_mods");
@@ -33,36 +37,50 @@ public class ModLoader {
                 }
             });
             ApiBase.theProfiler.endStartSection("scan_jars");
+            List<URL> loaderURLs = new ArrayList<URL>();
+            List<String> modClassNames = new ArrayList<String>();
             for(File modZip : zips){
                 ApiBase.theProfiler.startSection("jar_" + modZip.getName());
-                try{
-                    ClassLoader loader = new URLClassLoader(new URL[]{modZip.toURI().toURL()}, ModLoader.class.getClassLoader());
-                    ZipFile zipFile = new ZipFile(modZip);
-                    Enumeration<? extends ZipEntry> entries = zipFile.entries();
-                    while(entries.hasMoreElements()){
-                        ZipEntry entry = entries.nextElement();
-                        if(entry.getName().endsWith(".class")){
-                            Class modClass = loader.loadClass(entry.getName().replaceAll("/", ".").substring(0, entry.getName().length() - 6));
-                            if(Mod.class.isAssignableFrom(modClass)){
-                                modList.add(modClass);
-                                BlazeLoader.getLogger().logDetail("Loaded mod: [" + modClass.getName() + "] from zip: [" + modZip.getName() + "].");
-                            }
-                        }
-                    }
-                }catch(IOException e){
-                    BlazeLoader.getLogger().logWarning("Skipping corrupt zip: " + modZip.getName());
-                }catch(Exception e){
-                    BlazeLoader.getLogger().logWarning("Skipping corrupt mod in: " + modZip.getName());
-                }
+                loadZip(modZip, modClassNames, loaderURLs);
                 ApiBase.theProfiler.endSection();
+            }
+            ClassLoader loader = new URLClassLoader(loaderURLs.toArray(new URL[loaderURLs.size()]), ModLoader.class.getClassLoader());
+            for(String modClassName : modClassNames){
+                loadClass(modClassName, loader, modList);
             }
             ApiBase.theProfiler.endSection();
         }
         ApiBase.theProfiler.endSection();
     }
 
-    public static void loadModsToList(File searchDir){
-        loadMods(searchDir, ModList.getUnloadedMods());
+    private static void loadZip(File modZip, List<String> modClassNames, List<URL> loaderURLs){
+        try{
+            loaderURLs.add(modZip.toURI().toURL());
+            ZipFile zipFile = new ZipFile(modZip);
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while(entries.hasMoreElements()){
+                ZipEntry entry = entries.nextElement();
+                if(entry.getName().endsWith(".class")){
+                    modClassNames.add(entry.getName().replaceAll("/", ".").substring(0, entry.getName().length() - 6));
+                }
+            }
+        }catch(IOException e){
+            BlazeLoader.getLogger().logWarning("Skipping corrupt zip: " + modZip.getName());
+        }catch(Exception e){
+            BlazeLoader.getLogger().logWarning("Skipping corrupt mod in: " + modZip.getName());
+        }
+    }
+
+    private static void loadClass(String className, ClassLoader loader, List<Class> modList){
+        try{
+            Class modClass = loader.loadClass(className);
+            if(Mod.class.isAssignableFrom(modClass)){
+                modList.add(modClass);
+                BlazeLoader.getLogger().logDetail("Loaded mod: " + modClass.getName() + ".");
+            }
+        }catch(Exception e){
+            BlazeLoader.getLogger().logWarning("Skipping corrupt mod.");
+        }
     }
 
 }
