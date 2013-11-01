a50b033bfd1283af8b695873b329c91e0b649ab4
warriordog <acomputerdog@gmail.com>
2013-11-01 15:09:28 -0400

    Implement the wall

--- net/acomputerdog/BlazeLoader/main/Version.java
+++ net/acomputerdog/BlazeLoader/main/Version.java
@@ -22,7 +22,7 @@ public final class Version {
      * @return Get the version of the API features of BlazeLoader
      */
     public static int getApiVersion(){
-        return 3;
+        return 4;
     }
 
     /**
