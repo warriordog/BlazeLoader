7f131750d754e2fef476f6249c018fda5dc233fe
big_Xplosion <bigXplosion44@gmail.com>
2014-01-13 00:16:14 +0100

    Removed the MinecraftPackageAccessors.

--- net/minecraft/block/MinecraftPackageAccessorC.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.block;
-
-import net.minecraft.util.IIcon;
-
-public class MinecraftPackageAccessorC {
-    public static IIcon getBlockIcon(Block block) {
-        return block.field_149761_L;
-    }
-}
