1ccf6b73524a4fbd7bb8dd79e4e6cd3dac6957d4
warriordog <acomputerdog@gmail.com>
2014-01-15 21:09:32 -0500

    Add ApiItem.registerItem()

--- net/acomputerdog/BlazeLoader/api/item/ApiItem.java
+++ net/acomputerdog/BlazeLoader/api/item/ApiItem.java
@@ -1,9 +1,20 @@
 package net.acomputerdog.BlazeLoader.api.item;
 
 
+import net.minecraft.item.Item;
+
 /**
  * Api functions for items.
  */
 public class ApiItem {
 
+    /**
+     * Registers an item in the game registry.
+     * @param id The item ID
+     * @param name The item name
+     * @param item The item itself
+     */
+    public static void registerItem(int id, String name, Item item) {
+        Item.field_150901_e.func_148756_a(id, name, item);
+    }
 }
