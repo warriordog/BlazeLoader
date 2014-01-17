dd812da4273f51b0ab89a09381ea9d2f8bc5763b
Sollace <mwchris@mweb.co.za>
2014-01-17 15:11:17 +0200

    Update BlCraftingManager.java
    Fixed tab indents and cleaned up variable names

--- net/acomputerdog/BlazeLoader/api/recipe/BlCraftingManager.java
+++ net/acomputerdog/BlazeLoader/api/recipe/BlCraftingManager.java
@@ -61,9 +61,8 @@ public class BlCraftingManager {
 				recipeManagers.put(key, new ArrayList());
 			}
 			return recipeManagers.get(key);
-		} else {
-			return instance.getRecipeList();
 		}
+		return instance.getRecipeList();
 	}
 	
 	/**
@@ -114,66 +113,65 @@ public class BlCraftingManager {
 		return reci;
 	}
 	
-	private ShapedRecipes makeRecipe(ItemStack par1ItemStack, int reversable, Object ... par2ArrayOfObj) {
-        String var3 = "";
-        int var4 = 0, var5 = 0, var6 = 0;
+	private ShapedRecipes makeRecipe(ItemStack par1ItemStack, int reversable, Object ... inputObjects) {
+		String recipePattern = "";
+		int i = 0, len = 0, pointer = 0;
 
-        if (par2ArrayOfObj[var4] instanceof String[]) {
-            String[] var7 = (String[])((String[])par2ArrayOfObj[var4++]);
+		if (inputObjects[i] instanceof String[]) {
+			String[] inputStrings = (String[])((String[])inputObjects[i++]);
 
-            for (int var8 = 0; var8 < var7.length; ++var8) {
-                String var9 = var7[var8];
-                ++var6;
-                var5 = var9.length();
-                var3 = var3 + var9;
-            }
-        } else {
-            while (par2ArrayOfObj[var4] instanceof String) {
-                String var11 = (String)par2ArrayOfObj[var4++];
-                ++var6;
-                var5 = var11.length();
-                var3 = var3 + var11;
-            }
-        }
+			for (int k = 0; k < inputStrings.length; ++k) {
+				String inputString = inputStrings[k];
+				++pointer;
+				len = inputString.length();
+				recipePattern += inputString;
+			}
+		} else {
+			while (inputObjects[i] instanceof String) {
+				String inputPatternString = (String)inputObjects[i++];
+				++pointer;
+				len = inputPatternString.length();
+				recipePattern += inputPatternString;
+			}
+		}
 
-        HashMap var12;
+		HashMap inputMapping;
+		for (inputMapping = new HashMap(); i < inputObjects.length; i += 2) {
+			Character var13 = (Character)inputObjects[i];
+			ItemStack var14 = null;
 
-        for (var12 = new HashMap(); var4 < par2ArrayOfObj.length; var4 += 2) {
-            Character var13 = (Character)par2ArrayOfObj[var4];
-            ItemStack var14 = null;
+			if (inputObjects[i + 1] instanceof Item) {
+				var14 = new ItemStack((Item)inputObjects[i + 1]);
+			} else if (inputObjects[i + 1] instanceof Block) {
+				var14 = new ItemStack((Block)inputObjects[i + 1], 1, 32767);
+			} else if (inputObjects[i + 1] instanceof ItemStack) {
+				var14 = (ItemStack)inputObjects[i + 1];
+			}
 
-            if (par2ArrayOfObj[var4 + 1] instanceof Item) {
-                var14 = new ItemStack((Item)par2ArrayOfObj[var4 + 1]);
-            } else if (par2ArrayOfObj[var4 + 1] instanceof Block) {
-                var14 = new ItemStack((Block)par2ArrayOfObj[var4 + 1], 1, 32767);
-            } else if (par2ArrayOfObj[var4 + 1] instanceof ItemStack) {
-                var14 = (ItemStack)par2ArrayOfObj[var4 + 1];
-            }
+			inputMapping.put(var13, var14);
+		}
 
-            var12.put(var13, var14);
-        }
+		ItemStack[] inputStacks = new ItemStack[len * pointer];
 
-        ItemStack[] var15 = new ItemStack[var5 * var6];
+		for (int k = 0; k < len * pointer; ++k) {
+			char var10 = recipePattern.charAt(k);
 
-        for (int var16 = 0; var16 < var5 * var6; ++var16) {
-            char var10 = var3.charAt(var16);
+			if (inputMapping.containsKey(Character.valueOf(var10))) {
+				inputStacks[k] = ((ItemStack)inputMapping.get(Character.valueOf(var10))).copy();
+			} else {
+				inputStacks[k] = null;
+			}
+		}
 
-            if (var12.containsKey(Character.valueOf(var10))) {
-                var15[var16] = ((ItemStack)var12.get(Character.valueOf(var10))).copy();
-            } else {
-                var15[var16] = null;
-            }
-        }
-
-        if (reversable > 0) {
-        	ReversableShapedRecipe result = new ReversableShapedRecipe(var5, var6, var15, par1ItemStack);
-        	if (reversable == 2) {
-        		result.setReverseOnly();
-        	}
-        	return result;
-        }
-        return new BShapedRecipe(var5, var6, var15, par1ItemStack);
-    }
+		if (reversable > 0) {
+			ReversableShapedRecipe result = new ReversableShapedRecipe(len, pointer, inputStacks, par1ItemStack);
+			if (reversable == 2) {
+				result.setReverseOnly();
+			}
+			return result;
+		}
+		return new BShapedRecipe(len, pointer, inputStacks, par1ItemStack);
+	}
 	
 	/**
 	 * Adds a shapeless recipe to the normal crafting manager
@@ -193,105 +191,97 @@ public class BlCraftingManager {
 		getRecipeList().add(makeShapelessRecipe(stack, args));
 	}
 
-    private BShapelessRecipe makeShapelessRecipe(ItemStack par1ItemStack, Object ... par2ArrayOfObj) {
-        ArrayList var3 = new ArrayList();
-        Object[] var4 = par2ArrayOfObj;
-
-        for (Object var7 : par2ArrayOfObj) {
-            if (var7 instanceof ItemStack) {
-                var3.add(((ItemStack)var7).copy());
-            } else if (var7 instanceof Item) {
-                var3.add(new ItemStack((Item)var7));
-            } else {
-                if (!(var7 instanceof Block)) {
-                    throw new RuntimeException("Invalid shapeless recipe!");
-                }
-
-                var3.add(new ItemStack((Block)var7));
-            }
-        }
-
-        return new BShapelessRecipe(par1ItemStack, var3);
-    }
+	private BShapelessRecipe makeShapelessRecipe(ItemStack par1ItemStack, Object ... par2ArrayOfObj) {
+		ArrayList inputStacks = new ArrayList();
+		
+		for (Object inputObj : par2ArrayOfObj) {
+			if (inputObj instanceof ItemStack) {
+				inputStacks.add(((ItemStack)inputObj).copy());
+			} else if (inputObj instanceof Item) {
+				inputStacks.add(new ItemStack((Item)inputObj));
+			} else {
+				if (!(inputObj instanceof Block)) throw new RuntimeException("Invalid shapeless recipe!");
+				
+				inputStacks.add(new ItemStack((Block)inputObj));
+			}
+		}
+		
+		return new BShapelessRecipe(par1ItemStack, inputStacks);
+	}
     
-    /**
-     * 
+	/**
 	 * Gets the result of the recipe that matches the crafting table input
-     * @param par1InventoryCrafting
-     * @param par2World
-     * @return
-     */
-    public ItemStack findMatchingRecipe(InventoryCrafting par1InventoryCrafting, World par2World) {
-    	return findMatchingRecipe(par1InventoryCrafting, par2World, 3, 3);
-    }
+	 * @param craftingInventory
+	 * @param w
+	 * @return
+	 */
+	public ItemStack findMatchingRecipe(InventoryCrafting craftingInventory, World w) {
+		return findMatchingRecipe(craftingInventory, w, 3, 3);
+	}
     
-    /**
-     * Gets the result of the recipe that matches the crafting table input 
-     * @param par1InventoryCrafting
-     * @param par2World
-     * @param width: table width
-     * @param height: table height
-     * @return
-     */
-    public ItemStack findMatchingRecipe(InventoryCrafting par1InventoryCrafting, World par2World, int width, int height) {
-        int var3 = 0;
-        ItemStack var4 = null;
-        ItemStack var5 = null;
-        int var6;
+	/**
+	 * Gets the result of the recipe that matches the crafting table input 
+	 * @param craftingInventory
+	 * @param w
+	 * @param width: table width
+	 * @param height: table height
+	 * @return
+	 */
+	public ItemStack findMatchingRecipe(InventoryCrafting craftingInventory, World w, int width, int height) {
+		int i, pointer = 0;
+		ItemStack one = null, two = null;
+		
+		for (i = 0; i < craftingInventory.getSizeInventory(); ++i) {
+			ItemStack checkedStack = craftingInventory.getStackInSlot(i);
 
-        for (var6 = 0; var6 < par1InventoryCrafting.getSizeInventory(); ++var6) {
-            ItemStack var7 = par1InventoryCrafting.getStackInSlot(var6);
+			if (checkedStack != null) {
+				if (pointer == 0) {
+					one = checkedStack;
+				} else if (pointer == 1) {
+					two = checkedStack;
+				}
 
-            if (var7 != null) {
-                if (var3 == 0) {
-                    var4 = var7;
-                }
+				++pointer;
+			}
+		}
 
-                if (var3 == 1) {
-                    var5 = var7;
-                }
+		if (pointer == 2 && one == two && one.stackSize == 1 && two.stackSize == 1 && one.getItem().isDamageable()) {
+			Item itemOne = one.getItem();
+			int damageRemainder = itemOne.getMaxDamage() - one.getItemDamageForDisplay();
+			int otherDanageRemainder = itemOne.getMaxDamage() - two.getItemDamageForDisplay();
+			int damageOff = damageRemainder + otherDanageRemainder + itemOne.getMaxDamage() * 5 / 100;
+			int resultDamage = itemOne.getMaxDamage() - damageOff;
+	
+			if (resultDamage < 0) {
+				resultDamage = 0;
+			}
 
-                ++var3;
-            }
-        }
-
-        if (var3 == 2 && var4 == var5 && var4.stackSize == 1 && var5.stackSize == 1 && var4.getItem().isDamageable()) {
-            Item var11 = var4.getItem();
-            int var13 = var11.getMaxDamage() - var4.getItemDamageForDisplay();
-            int var8 = var11.getMaxDamage() - var5.getItemDamageForDisplay();
-            int var9 = var13 + var8 + var11.getMaxDamage() * 5 / 100;
-            int var10 = var11.getMaxDamage() - var9;
-
-            if (var10 < 0) {
-                var10 = 0;
-            }
-
-            return new ItemStack(var4.getItem(), 1, var10);
-        } else {
+			return new ItemStack(one.getItem(), 1, resultDamage);
+		}
+        
         	List<IRecipe> recipes = getRecipeList();
-            for (IRecipe var12 : recipes) {
-            	if (var12 instanceof BShapedRecipe) {
-            		((BShapedRecipe)var12).setCraftingSize(width, height);
-            	} else if (var12 instanceof BShapelessRecipe) {
-            		((BShapelessRecipe)var12).setCraftingSize(width, height);
-            	}
+		for (IRecipe var12 : recipes) {
+			if (var12 instanceof BShapedRecipe) {
+				((BShapedRecipe)var12).setCraftingSize(width, height);
+			} else if (var12 instanceof BShapelessRecipe) {
+				((BShapelessRecipe)var12).setCraftingSize(width, height);
+			}
             	
-                if (var12.matches(par1InventoryCrafting, par2World)) {
-                    return var12.getCraftingResult(par1InventoryCrafting);
-                }
-            }
+			if (var12.matches(craftingInventory, w)) {
+				return var12.getCraftingResult(craftingInventory);
+			}
+		}
 
-            return null;
-        }
-    }
+		return null;
+	}
     
-    /**
-     * Gets the input items for recipe that has the matching result
-     * @param result
-     * @param width: table width
-     * @param height: table height
-     * @return
-     */
+	/**
+	 * Gets the input items for recipe that has the matching result
+	 * @param result
+	 * @param width: table width
+	 * @param height: table height
+	 * @return
+	 */
 	public ItemStack[] getRecipeInput(ItemStack result, int width, int height) {
 		List<IRecipe> recipes = getRecipeList();
 		for (IRecipe i : recipes) {
@@ -312,11 +302,11 @@ public class BlCraftingManager {
 	 * @param resultItem
 	 * @param totalRemovals
 	 */
-    public static void RemoveVanillaRecipe(ItemStack resultItem, int totalRemovals) {
+	public static void RemoveVanillaRecipe(ItemStack resultItem, int totalRemovals) {
 		RemoveRecipe(resultItem, totalRemovals, VanillaCraftingManagerKey);
 	}
 	
-    /**
+	/**
 	 * Removes a recipe from this crafting manager
 	 * @param resultItem
 	 * @param totalRemovals
@@ -327,7 +317,7 @@ public class BlCraftingManager {
 	
 	/**
 	 * Code by yope_fried inspired by pigalot, modified by awr
-     */
+	*/
 	private static void RemoveRecipe(ItemStack resultItem, int totalRemovals, int recipeManagerId) {
 		List<IRecipe> recipes;
 		
@@ -336,24 +326,24 @@ public class BlCraftingManager {
 		} else {
 			recipes = BlCraftingManager.getInstance(recipeManagerId).getRecipeList();
 		}
-        int count = 0;
+		int count = 0;
 
-        for (int i = 0; i < recipes.size(); i++) {
-            IRecipe tmpRecipe = recipes.get(i);
+		for (int i = 0; i < recipes.size(); i++) {
+			IRecipe tmpRecipe = recipes.get(i);
 
-            if (tmpRecipe instanceof ShapedRecipes) {
-                ShapedRecipes recipe = (ShapedRecipes)tmpRecipe;
-                ItemStack recipeResult = recipe.getRecipeOutput();
+			if (tmpRecipe instanceof ShapedRecipes) {
+				ShapedRecipes recipe = (ShapedRecipes)tmpRecipe;
+				ItemStack recipeResult = recipe.getRecipeOutput();
 
-                if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
-                    recipes.remove(i--);
-                    count++;
+				if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
+					recipes.remove(i--);
+					count++;
 
-                    if (count == totalRemovals) {
-                        return;
-                    }
-                }
-            }
-        }
+					if (count == totalRemovals) {
+						return;
+					}
+				}
+			}
+		}
 	}
 }
