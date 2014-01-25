1c7f9e3290f4f4dd0d7ec9d7fd5d362187b0a672
warriordog <acomputerdog@gmail.com>
2014-01-25 10:13:00 -0500

    * Fix over-complicated file copy method * No longer cancels apply_at if a library was not found (in case it was provided) * Uses newlines instead of "---" as a divider.

--- apply_at.bat
+++ apply_at.bat
@@ -3,45 +3,45 @@ set version=1.7.2
 if not exist .\jars\versions\%version%\%version%.json (
     echo Copying %version%.json...
     if exist %appdata%\.minecraft\versions\%version%\%version%.json (
-        echo f|xcopy "%appdata%\.minecraft\versions\%version%\%version%.json" ".\jars\versions\%version%\%version%.json" /Y
+        if not exist ".\jars\versions\%version%/" mkdir ".\jars\versions\%version%/"
+        xcopy /I /Y "%appdata%\.minecraft\versions\%version%\%version%.json" ".\jars\versions\%version%\"
         echo done.
-        echo ---
+        echo.
     ) else (
-        echo %version%.json not found!  Aborting!
-        exit /b 1
+        echo %version%.json not found!
     )
 )
 if not exist .\jars\versions\%version%\%version%.jar (
     echo Copying %version%.jar...
     if exist %appdata%\.minecraft\versions\%version%\%version%.jar (
-        echo f|xcopy "%appdata%\.minecraft\versions\%version%\%version%.jar" ".\jars\versions\%version%\%version%.jar" /Y
+        if not exist ".\jars\versions\%version%/" mkdir ".\jars\versions\%version%/"
+        xcopy /I /Y "%appdata%\.minecraft\versions\%version%\%version%.jar" ".\jars\versions\%version%\"
         echo done.
-        echo ---
+        echo.
     ) else (
-        echo %version%.jar not found!  Aborting!
-        exit /b 1
+        echo %version%.jar not found!
     )
 )
 if not exist .\jars\libraries\net\minecraft\launchwrapper\1.9\launchwrapper-1.9.jar (
     echo Copying launchwrapper-1.9.jar...
     if exist %appdata%\.minecraft\libraries\net\minecraft\launchwrapper\1.9\launchwrapper-1.9.jar (
-        echo f|xcopy "%appdata%\.minecraft\libraries\net\minecraft\launchwrapper\1.9\launchwrapper-1.9.jar" ".\jars\libraries\net\minecraft\launchwrapper\1.9\launchwrapper-1.9.jar" /Y
+        if not exist ".\jars\libraries\net\minecraft\launchwrapper\1.9/" mkdir ".\jars\libraries\net\minecraft\launchwrapper\1.9/"
+        xcopy /I /Y "%appdata%\.minecraft\libraries\net\minecraft\launchwrapper\1.9\launchwrapper-1.9.jar" ".\jars\libraries\net\minecraft\launchwrapper\1.9\"
         echo done.
-        echo ---
+        echo.
     ) else (
-        echo launchwrapper-1.9.jar not found!  Aborting!
-        exit /b 1
+        echo launchwrapper-1.9.jar not found!
     )
 )
 if not exist .\jars\libraries\org\ow2\asm\asm-debug-all\4.1\asm-debug-all-4.1.jar (
     echo Copying asm-debug-all-4.1.jar...
     if exist %appdata%\.minecraft\libraries\org\ow2\asm\asm-debug-all\4.1\asm-debug-all-4.1.jar (
-        echo f|xcopy "%appdata%\.minecraft\libraries\org\ow2\asm\asm-debug-all\4.1\asm-debug-all-4.1.jar" ".\jars\libraries\org\ow2\asm\asm-debug-all\4.1\asm-debug-all-4.1.jar" /Y
+        if not exist ".\jars\libraries\org\ow2\asm\asm-debug-all\4.1/" mkdir ".\jars\libraries\org\ow2\asm\asm-debug-all\4.1/"
+        xcopy /I /Y "%appdata%\.minecraft\libraries\org\ow2\asm\asm-debug-all\4.1\asm-debug-all-4.1.jar" ".\jars\libraries\org\ow2\asm\asm-debug-all\4.1\"
         echo done.
-        echo ---
+        echo.
     ) else (
-        echo asm-debug-all-4.1.jar not found!  Aborting!
-        exit /b 1
+        echo asm-debug-all-4.1.jar not found!
     )
 )
 cd BlazeLoader
