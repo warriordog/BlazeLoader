058d328e0eb1fbd876d48558d7c279040e9cb81c
warriordog <acomputerdog@gmail.com>
2014-07-15 14:50:35 -0400

    Remove some deprecated settings

--- src/net/acomputerdog/BlazeLoader/main/Settings.java
+++ src/net/acomputerdog/BlazeLoader/main/Settings.java
@@ -8,16 +8,6 @@ import net.acomputerdog.core.logger.ELogLevel;
 public class Settings {
 
     /**
-     * The folder where mods are loaded from.  Relative to minecraft's working directory.
-     */
-    public static String modDir = "/BL/mods/";
-
-    /**
-     * Option to disable loading mods.  Set to false and modloading will be skipped.
-     */
-    public static boolean enableMods = true;
-
-    /**
      * The folder where config files are stored.  Mods do not have to obey this, but should if possible.
      */
     public static String configDir = "/BL/config/";
@@ -26,9 +16,4 @@ public class Settings {
      * The minimum log level required for a message to be displayed.
      */
     public static String minimumLogLevel = "DEBUG";
-
-    /**
-     * If disabled, BlazeLoader will not load mods from the classpath.
-     */
-    public static boolean loadModsFromClasspath = true;
 }
