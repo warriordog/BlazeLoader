6f8a015899d51be99e7250ce10f28dfc4dd61b9d
warriordog <acomputerdog@gmail.com>
2014-07-19 17:46:27 -0400

    Refactor BLEventInjectionTransformer to simplify the creation of events

--- src/net/acomputerdog/BlazeLoader/transformers/BLEventInjectionTransformer.java
+++ src/net/acomputerdog/BlazeLoader/transformers/BLEventInjectionTransformer.java
@@ -8,10 +8,15 @@ import com.mumfrey.liteloader.transformers.event.inject.MethodHead;
 import net.acomputerdog.BlazeLoader.util.obf.BLMethodInfo;
 import net.acomputerdog.BlazeLoader.util.obf.BLOBF;
 
+import java.util.regex.Pattern;
+
 /**
  * Injects events into MC classes
  */
 public class BLEventInjectionTransformer extends EventInjectionTransformer {
+    private static final String EVENT_HANDLER = "net.acomputerdog.BlazeLoader.event.EventHandler";
+    private static final String PREFIX = Pattern.quote("BL.");
+
     /**
      * Subclasses should register events here
      */
@@ -24,15 +29,23 @@ public class BLEventInjectionTransformer extends EventInjectionTransformer {
 
             InjectionPoint methodHead = new MethodHead();
 
-            this.addEvent(Event.getOrCreate("BL.loadWorld", false), loadWorld, methodHead)
-                    .addListener(new MethodInfo("net.acomputerdog.BlazeLoader.event.EventHandler", "eventLoadWorld"));
-            this.addEvent(Event.getOrCreate("BL.profilerStart", false), startSection, methodHead)
-                    .addListener(new MethodInfo("net.acomputerdog.BlazeLoader.event.EventHandler", "eventProfilerStart"));
-            this.addEvent(Event.getOrCreate("BL.profilerEnd", false), endSection, methodHead)
-                    .addListener(new MethodInfo("net.acomputerdog.BlazeLoader.event.EventHandler", "eventProfilerEnd"));
+            this.addBLEvent("BL.eventLoadWorld", loadWorld, methodHead);
+            this.addBLEvent("BL.eventProfilerStart", startSection, methodHead);
+            this.addBLEvent("BL.eventProfilerEnd", endSection, methodHead);
         } catch (Exception e) {
             System.err.println("A fatal exception occurred while injecting BlazeLoader!  BlazeLoader will not be able to run!");
             throw new RuntimeException("Exception injecting BlazeLoader!", e);
         }
+    }
+
+    private void addBLEvent(String name, MethodInfo method, InjectionPoint injectionPoint) {
+        this.addEvent(Event.getOrCreate(name, false), method, injectionPoint).addListener(new MethodInfo(EVENT_HANDLER, stripPrefix(name)));
+    }
+
+    private String stripPrefix(String name) {
+        if (name == null) {
+            return null;
+        }
+        return name.replaceFirst(PREFIX, "");
     }
 }
