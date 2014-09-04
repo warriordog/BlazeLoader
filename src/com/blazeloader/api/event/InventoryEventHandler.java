7377f21bc34f5e33c5db34c3e9a0126b8ad06baf
warriordog <acomputerdog@gmail.com>
2014-09-04 15:43:17 -0400

    Move BlazeLoader from net.acomputerdog.BlazeLoader to com.blazeloader.api

---
+++ src/com/blazeloader/api/event/InventoryEventHandler.java
@@ -0,0 +1,10 @@
+package com.blazeloader.api.event;
+
+import com.blazeloader.api.mod.BLMod;
+
+/**
+ * Interface for mods that handle inventory events
+ */
+public interface InventoryEventHandler extends BLMod {
+
+}
---
+++ src/com/blazeloader/api/mod/BLModResourcePack.java
@@ -0,0 +1,41 @@
+package com.blazeloader.api.mod;
+
+import net.minecraft.client.resources.FileResourcePack;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A ResourcePack containing mod resources
+ */
+public class BLModResourcePack extends FileResourcePack {
+    public ModData mod;
+    private final byte[] bytes;
+
+    public BLModResourcePack(ModData mod) {
+        super(mod.getModSource());
+        this.mod = mod;
+        this.bytes = (
+                "{\n" +
+                        " \"pack\": {\n" +
+                        "   \"description\": \"Generated pack for Mod " + this.mod.getModId() + "\",\n" +
+                        "   \"pack_format\": 1\n" +
+                        "}\n" +
+                        "}").getBytes();
+    }
+
+    @Override
+    public String getPackName() {
+        return "BLModResourcePack:" + this.mod.getModId();
+    }
+
+    @Override
+    protected InputStream getInputStreamByName(String name) throws IOException {
+        if (name.equals("pack.mcmeta") && !super.hasResourceName("pack.mcmeta")) {
+            return new ByteArrayInputStream(bytes);
+        } else {
+            return super.getInputStreamByName(name);
+        }
+    }
+}
