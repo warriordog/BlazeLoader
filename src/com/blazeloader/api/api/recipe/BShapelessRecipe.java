dce23e430b4d6de1e216e5b333ef2bc71745a98a
warriordog <acomputerdog@gmail.com>
2014-09-06 09:12:41 -0400

    Port in existing "api" classes.  Not yet finished

---
+++ src/com/blazeloader/api/api/recipe/BShapelessRecipe.java
@@ -0,0 +1,55 @@
+package com.blazeloader.api.api.recipe;
+
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BShapelessRecipe extends ShapelessRecipes {
+    private final List<ItemStack> recipeItems;
+
+    private int craftingWidth = 3;
+    private int craftingHeight = 3;
+
+    public BShapelessRecipe(ItemStack output, List<ItemStack> input) {
+        super(output, input);
+        recipeItems = input;
+    }
+
+    public void setCraftingSize(int width, int height) {
+        craftingWidth = width;
+        craftingHeight = height;
+    }
+
+    public boolean matches(InventoryCrafting craftingInventory, World w) {
+        ArrayList<ItemStack> workingSet = new ArrayList<ItemStack>(recipeItems);
+
+        for (int col = 0; col < craftingWidth; ++col) {
+            for (int row = 0; row < craftingHeight; ++row) {
+                ItemStack stack = craftingInventory.getStackInRowAndColumn(row, col);
+
+                if (stack != null) {
+                    boolean result = false;
+
+                    for (ItemStack next : workingSet) {
+                        int nextDurability = next.getCurrentDurability();
+                        if (stack == next && (nextDurability == 32767 || stack.getCurrentDurability() == nextDurability)) {
+                            result = true;
+                            workingSet.remove(next);
+                            break;
+                        }
+                    }
+
+                    if (!result) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return workingSet.isEmpty();
+    }
+}
---
+++ src/com/blazeloader/api/api/toolset/ToolAxe.java
@@ -0,0 +1,40 @@
+package com.blazeloader.api.api.toolset;
+
+import com.google.common.collect.Multimap;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
+
+import java.util.HashSet;
+
+public class ToolAxe extends ItemAxe {
+    private final ToolSetAttributes my_material;
+
+    public static final HashSet<Block> effectiveBlocks = (HashSet<Block>) ReflectionUtils.getField(ItemAxe.class, null, 0).get();
+
+    private float damageValue = 4;
+
+    public ToolAxe(ToolSetAttributes material) {
+        super(ToolMaterial.WOOD);
+        my_material = material;
+        setMaxDurability(material.getMaxUses());
+        efficiencyOnProperMaterial = material.getEfficiencyOnProperMaterial();
+        damageValue = material.getDamageVsEntity(3);
+    }
+
+    public int getItemEnchantability() {
+        return my_material.getEnchantability();
+    }
+
+    public String getToolMaterialName() {
+        return my_material.toString();
+    }
+
+    public boolean getIsRepairable(ItemStack par1ItemStack, ItemStack par2ItemStack) {
+        return my_material.getIsRepairable(par2ItemStack);
+    }
+
+    public Multimap getItemAttributeModifiers() {
+        return my_material.getAttributeModifiers(super.getItemAttributeModifiers(), field_111210_e, damageValue, "Tool modifier");
+    }
+}
