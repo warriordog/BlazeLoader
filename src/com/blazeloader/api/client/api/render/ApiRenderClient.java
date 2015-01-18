a1bbf04d62295c51101e12aaed26efe4db83b5d7
warriordog <acomputerdog@gmail.com>
2015-01-18 09:31:56 -0500

    Expand ApiRenderClient to help with drawing shapes

--- src/com/blazeloader/api/client/api/render/ApiRenderClient.java
+++ src/com/blazeloader/api/client/api/render/ApiRenderClient.java
@@ -2,27 +2,33 @@ package com.blazeloader.api.client.api.render;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
 
 /**
  * Contains functions related to game rendering.
  */
 public class ApiRenderClient {
+    private static int lastWidth = -1;
+    private static int lastHeight = -1;
+    private static int lastScale = -1;
+    private static ScaledResolution scale = null;
 
     /**
-     * Generates a FontRenderer-compatible ARBG color int.
+     * Generates a FontRenderer-compatible ARGB color int.
      *
      * @param alpha Alpha-channel value.
      * @param red   Red-channel value.
      * @param green Green-channel value.
      * @param blue  Blue-channel value.
-     * @return Return the alpha, red, green, and blue compressed into an ARBG int.
+     * @return Return the alpha, red, green, and blue compressed into an ARGB int.
      */
     public static int getARGB(int alpha, int red, int green, int blue) {
         int arbg = 0;
         arbg |= (alpha & 255) << 24;
         arbg |= (red & 255) << 16;
-        arbg |= (blue & 255) << 8;
-        arbg |= (green & 255);
+        arbg |= (green & 255) << 8;
+        arbg |= (blue & 255);
         return arbg;
     }
 
@@ -67,5 +73,99 @@ public class ApiRenderClient {
      */
     public static void drawString(String string, int x, int y, int color) {
         drawString(string, x, y, color, false, false);
+    }
+
+    /**
+     * Draws a rectangle onto the screen.
+     *
+     * @param x      The x-coordinate
+     * @param y      The y-coordinate
+     * @param width  The width
+     * @param height The height
+     * @param color  The ARGB color
+     */
+    public static void drawRect(int x, int y, int width, int height, int color) {
+        Gui.drawRect(x, y, x + width, y + height, color);
+    }
+
+    /**
+     * Draws a square onto the screen.
+     *
+     * @param x     The x-coordinate
+     * @param y     The y-coordinate
+     * @param size  The length of each side
+     * @param color The ARGB color
+     */
+    public static void drawSquare(int x, int y, int size, int color) {
+        drawRect(x, y, size, size, color);
+    }
+
+    /**
+     * Draws a horizontal line onto the screen.
+     *
+     * @param x      The x-coordinate
+     * @param y      The y-coordinate
+     * @param length The length
+     * @param color  The ARGB color
+     */
+    public static void drawHLine(int x, int y, int length, int color) {
+        drawRect(x, y, length, 1, color);
+    }
+
+    /**
+     * Draws a vertical line onto the screen.
+     *
+     * @param x      The x-coordinate
+     * @param y      The y-coordinate
+     * @param length The length
+     * @param color  The ARGB color
+     */
+    public static void drawVLine(int x, int y, int length, int color) {
+        drawRect(x, y, 1, length, color);
+    }
+
+    /**
+     * Draws a point on the screen
+     *
+     * @param x     The x-coordinate
+     * @param y     The y-coordinate
+     * @param color The ARGB color
+     */
+    public static void drawPoint(int x, int y, int color) {
+        drawRect(x, y, 1, 1, color);
+    }
+
+    /**
+     * Gets the scaled width of the screen
+     *
+     * @return return the scaled width of the screen as returned by a ScaledResolution
+     */
+    public static int getScaledWidth() {
+        return getScaledResolution().getScaledWidth();
+    }
+
+    /**
+     * Gets the scaled height of the screen
+     *
+     * @return return the scaled height of the screen as returned by a ScaledResolution
+     */
+    public static int getScaledHeight() {
+        return getScaledResolution().getScaledWidth();
+    }
+
+    /**
+     * Gets a shared, updated instance of ScaledResolution that can be used to get screen scale
+     *
+     * @return Return a scaled, correct instance of ScaledResolution
+     */
+    public static ScaledResolution getScaledResolution() {
+        Minecraft minecraft = Minecraft.getMinecraft();
+        if (scale == null || minecraft.displayWidth != lastWidth || minecraft.displayHeight != lastHeight || minecraft.gameSettings.guiScale != lastScale) {
+            lastWidth = minecraft.displayWidth;
+            lastHeight = minecraft.displayHeight;
+            lastScale = minecraft.gameSettings.guiScale;
+            scale = new ScaledResolution(minecraft, lastWidth, lastHeight);
+        }
+        return scale;
     }
 }
