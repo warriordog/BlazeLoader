466994e4bd8d9a5f1094c57138aeb8e7444d1362
Sollace <mwchris@mweb.co.za>
2015-06-06 14:36:57 +0200

    Adjusted some docs Implemented IStringable for UpdateType Added a subtract method to UpdateType

--- src/com/blazeloader/api/block/UpdateType.java
+++ src/com/blazeloader/api/block/UpdateType.java
@@ -1,9 +1,11 @@
 package com.blazeloader.api.block;
 
+import com.blazeloader.util.config.IStringable;
+
 /**
  * Notification flags for block set events. Can be added together.
  */
-public class UpdateType implements Cloneable {
+public class UpdateType implements Cloneable, IStringable<UpdateType> {
 	/**
 	 * Do not send any updates or notifications.
 	 * <p>
@@ -27,7 +29,8 @@ public class UpdateType implements Cloneable {
 	 * Send a block update and notify clients.
 	 * <p>
 	 * This is the default for most uses and equivalent to:
-	 * <i><br>BLOCK_UPDATE.and(NOTIFY_CLIENT)</i>
+	 * <br>
+	 * {@code BLOCK_UPDATE.and(NOTIFY_CLIENT)}
 	 */
 	public static final UpdateType UPDATE_AND_NOTIFY = new ConstUpdateType(0x011);//3
 	
@@ -38,7 +41,7 @@ public class UpdateType implements Cloneable {
 	}
 	
     /**
-     * Adds the flags of the given NotificationType to this one.
+     * Adds the flags of the given UpdateType to this one.
      * <p>Note that the default, constant values are read only and thus will not be modified. It is recommended to always use the returned value.
      * 
      * @param other The UpdateType to add to this one
@@ -47,13 +50,28 @@ public class UpdateType implements Cloneable {
      */
     public UpdateType and(UpdateType other) {
     	if (!has(other)) {
-    		value = value | other.value;
+    		value |= other.value;
     	}
     	return this;
     }
     
     /**
-     * Flips this NotificationType2 to represent the inverse of its current value
+     * Removes the given UpdateType from this one.
+     * <p>Note that the default, constant values are read only and thus will not be modified. It is recommended to always use the returned value.
+     * 
+     * @param other The UpdateType to subtract from this one
+     * 
+     * @returns the resulting UpdateType for chaining
+     */
+    public UpdateType subtract(UpdateType other) {
+    	if (has(other)) {
+    		value &= ~other.value;
+    	}
+    	return this;
+    }
+    
+    /**
+     * Flips this UpdateType to represent the inverse of its current value
      * <p>Note that the default, constant values are read only and thus will not be modified. It is recommended to always use the returned value.
      * 
      * @returns the resulting UpdateType for chaining
@@ -84,6 +102,38 @@ public class UpdateType implements Cloneable {
     	}
         return false;
     }
+    
+    public Object clone() {
+    	return new UpdateType(value);
+    }
+    
+    public String toString() {
+    	String result = "";
+    	if (has(UPDATE_AND_NOTIFY)) {
+    		result += " UPDATE_AND_NOTIFY";
+    	} else {
+			if (has(BLOCK_UPDATE)) result += " BLOCK_UPDATE";
+			if (has(NOTIFY_CLIENT)) result += " NOTIFY_CLIENT";
+    	}
+    	if (has(PREVENT_RERENDER)) result += " PREVENT_RERENDER";
+    	return result.trim();
+    }
+    
+	@Override
+	public UpdateType fromString(String string) {
+		int value = NONE.value;
+		if (string != null) {
+			string = string.trim().toUpperCase();
+			if (string.contains("UPDATE_AND_NOTIFY")) {
+				value |= UPDATE_AND_NOTIFY.value;
+			} else {
+				if (string.contains("BLOCK_UPDATE")) value |= BLOCK_UPDATE.value;
+				if (string.contains("NOTIFY_CLIENT")) value |= NOTIFY_CLIENT.value;
+			}
+			if (string.contains("PREVENT_RERENDER")) value |= PREVENT_RERENDER.value;
+		}
+		return new UpdateType(value);
+	}
 
     private static class ConstUpdateType extends UpdateType {
     	private ConstUpdateType(int flag) {
@@ -91,10 +141,17 @@ public class UpdateType implements Cloneable {
     	}
     	
     	/**
-    	 * No modifications allowed.<p>Instead creates a new one with the combined values.
+    	 * No modifications allowed.<p>Instead creates a new one with the modified value.
     	 */
         public UpdateType and(UpdateType other) {
         	return has(other) ? this : new UpdateType(value | other.value);
+        }
+        
+    	/**
+    	 * No modifications allowed.<p>Instead creates a new one with the modified value.
+    	 */
+        public UpdateType subtract(UpdateType other) {
+        	return has(other) ? new UpdateType(value & ~other.value) : this;
         }
         
     	/**
